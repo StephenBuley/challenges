3c06a74c19df9a30a03c6a1c9242a0d22e50c8ea
StephenBuley <65041377+StephenBuley@users.noreply.github.com>
2020-12-16 12:37:37 -0500

    Refactor #1
    Refactored some of the JS and CSS to be clearer and simpler.

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
 const faqs = Array.from(document.querySelectorAll(".faq"))
-const questions = Array.from(document.querySelectorAll(".question"))
-const downArrows = Array.from(document.querySelectorAll(".down-arrow"))
-const answers = Array.from(document.querySelectorAll(".answer"))
 
 faqs.forEach((faq, i) => {
     faq.addEventListener("click", () => {
         if (faq.classList.contains("open")) {
             faq.classList.remove("open")
-            questions[i].classList.remove("open")
-            downArrows[i].classList.remove("open")
-            answers[i].classList.remove("open")
         } else {
             faq.classList.add("open")
-            questions[i].classList.add("open")
-            downArrows[i].classList.add("open")
-            answers[i].classList.add("open")
         }
     })
 })
